7c936affea4044301062edd071543f768c8948ec
José Alejandro Gutiérrez Rodríguez <jalejandrogtzrdz@gmail.com>
2025-08-22 22:08:29 -0600

    fix: add vercel.json for SPA routing2

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    sourcemap: false
+  },
   server: {
     host: true,   // <--- importante
     port: 5173,   // puedes cambiarlo si quieres
